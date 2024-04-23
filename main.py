79bfcc714c22c3f47b3568780ca2f103c605ebb0
snakby99 <117627772+snakby99@users.noreply.github.com>
2024-04-23 23:24:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ class Food(BaseModel):
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost", "http://localhost:8080", "http://127.0.0.1", "http://127.0.0.1:8080"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],

8fc3510fa952099bcad67123e65f221cd49ba9cd
Pondz <142286063+MrPondzleep@users.noreply.github.com>
2024-05-20 15:48:15 +0700

    update

--- main.py
+++ main.py
@@ -23,14 +23,14 @@ import os
 
 app = FastAPI()
 
-# origins = [
-#     "http://localhost:5173",
-#     # เพิ่ม origins อื่นๆ ที่คุณต้องการเชื่อมต่อได้ที่นี่
-# ]
+origins = [
+    "*",
+    # เพิ่ม origins อื่นๆ ที่คุณต้องการเชื่อมต่อได้ที่นี่
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
